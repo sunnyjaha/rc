1fb02f579a2fb7419c07a0aad4caaf3701a3b105
anuragmirrorleech <115845369+anuragmirrorleech@users.noreply.github.com>
2023-01-22 21:52:11 +0530

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -1,8 +1,8 @@
 import pymongo 
 import os
 from helper.date import add_date
-DB_NAME = os.environ.get("DB_NAME","")
-DB_URL = os.environ.get("DB_URL","")
+DB_NAME = os.environ.get("DB_NAME","rename")
+DB_URL = os.environ.get("DB_URL","mongodb+srv://rename:rename@cluster0.f0vmafy.mongodb.net/?retryWrites=true&w=majority")
 mongo = pymongo.MongoClient(DB_URL)
 db = mongo[DB_NAME]
 dbcol = db["user"]

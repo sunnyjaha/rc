c4bcd7fd025b45052683a8fc7f8889efa6f283e8
anuragmirrorleech <115845369+anuragmirrorleech@users.noreply.github.com>
2023-01-22 22:13:49 +0530

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -1,6 +1,6 @@
 import os 
 from pyrogram import Client, filters
-token = os.environ.get('TOKEN','')
+token = os.environ.get('TOKEN','5958312899:AAHiq4rgI9nJF0daMC5af4Wf_off-nQQHR8')
 botid = token.split(':')[0]
 from helper.database import botdata, find_one, total_user
 from helper.progress import humanbytes
